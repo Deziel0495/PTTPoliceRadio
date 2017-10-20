0c048e58e6f394ea19597881350e6ef11dbef531
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-20 16:36:40 -0300

    Update client.lua

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -5,13 +5,29 @@ function loadAnimDict( dict )
     end
 end
 
+skins = {
+	-- Police --
+	GetHashKey("s_m_y_cop_01"),
+	GetHashKey("s_f_y_cop_01"),
+	-- Highway --
+	GetHashKey("s_m_y_hwaycop_01"),
+	-- Sheriff --
+	GetHashKey("s_m_y_sheriff_01"),
+	GetHashKey("s_f_y_sheriff_01"),
+	-- SECURITY --
+	GetHashKey("s_m_m_security_01"),
+	-- Ranger --
+	GetHashKey("s_m_y_ranger_01"),
+	GetHashKey("s_f_y_ranger_01"),
+}
+
 Citizen.CreateThread( function()
     while true do 
         Citizen.Wait( 0 )
 
         local ped = GetPlayerPed( -1 )
 
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then 
             DisableControlAction( 0, 19, true ) -- INPUT_CHARACTER_WHEEL (LEFTALT)  
 
             if ( not IsPauseMenuActive() ) then 
@@ -21,19 +37,19 @@ Citizen.CreateThread( function()
                         Citizen.Wait( 100 )
                     end 
                         if ( IsDisabledControlJustReleased( 0, 19 ) ) then
-						ClearPedSecondaryTask(ped)
+                        ClearPedSecondaryTask(ped)
                         SetEnableHandcuffs(ped, false)
                     else
-                        if ( IsDisabledControlJustPressed( 0, 19 ) ) then
-						ClearPedSecondaryTask(ped)
+                        if ( IsDisabledControlJustPressed( 0, 19 ) ) and checkskin() then
+                        ClearPedSecondaryTask(ped)
                         TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-						SetEnableHandcuffs(ped, true)
+                        SetEnableHandcuffs(ped, true)
                     end 
-					   if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
-					   DisableControlAction(1, 140, true)
-					   DisableControlAction(1, 141, true)
-					   DisableControlAction(1, 142, true)
-					end
+                       if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
+                       DisableControlAction(1, 140, true)
+                       DisableControlAction(1, 141, true)
+                       DisableControlAction(1, 142, true)
+                    end
                 end
             end 
         end 
@@ -53,7 +69,7 @@ Citizen.CreateThread( function()
 
         local ped = GetPlayerPed( -1 )
 
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then 
             DisableControlAction( 0, 36, true ) -- INPUT_DUCK (LEFTCTRL)  
 
             if ( not IsPauseMenuActive() ) then 
@@ -63,19 +79,28 @@ Citizen.CreateThread( function()
                         Citizen.Wait( 100 )
                     end 
                         if ( IsDisabledControlJustReleased( 0, 36 ) ) then
-						ClearPedSecondaryTask(ped)
+                        ClearPedSecondaryTask(ped)
                     else
                         if ( IsDisabledControlJustPressed( 0, 36 ) ) then
-						ClearPedSecondaryTask(ped)
+                        ClearPedSecondaryTask(ped)
                         TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                     end 
-					   if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
-					   DisableControlAction(1, 140, true)
-					   DisableControlAction(1, 141, true)
-					   DisableControlAction(1, 142, true)
-					end
+                       if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
+                       DisableControlAction(1, 140, true)
+                       DisableControlAction(1, 141, true)
+                       DisableControlAction(1, 142, true)
+                    end
                 end
             end 
         end 
     end
 end )
+
+function checkskin()
+    for i = 1, #skins do
+        if skins[i] == GetEntityModel(PlayerPedId()) then
+            return true
+        end
+    end
+    return false
+end

66a13260af0c838441f9514eb2e39b06488451ab
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-20 01:49:59 -0300

    Fixed unable to sprint - Found different animation

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -1,13 +1,13 @@
 function loadAnimDict( dict )
     while ( not HasAnimDictLoaded( dict ) ) do
         RequestAnimDict( dict )
-        Citizen.Wait( 5 )
+        Citizen.Wait( 0 )
     end
 end
 
 Citizen.CreateThread( function()
     while true do 
-        Citizen.Wait( 1 )
+        Citizen.Wait( 0 )
 
         local ped = GetPlayerPed( -1 )
 
@@ -20,17 +20,16 @@ Citizen.CreateThread( function()
                     while ( not HasAnimDictLoaded( "random@arrests" ) ) do 
                         Citizen.Wait( 100 )
                     end 
-                        if ( IsDisabledControlJustReleased( 0, 19 ) ) then 
+                        if ( IsDisabledControlJustReleased( 0, 19 ) ) then
                         SetEnableHandcuffs(ped, false)
                         ClearPedSecondaryTask(ped)
                     else
                         if ( IsDisabledControlJustPressed( 0, 19 ) ) then
-                        ClearPedSecondaryTask(ped)
-                        SetEnableHandcuffs(ped, true)
                         DisableControlAction(1, 140, true)
                         DisableControlAction(1, 141, true)
                         DisableControlAction(1, 142, true)
-                        TaskPlayAnim(ped, "random@arrests", "generic_radio_chatter", 8.0, 3.0, -1, 49, 0, 0, 0, 0 )
+						SetEnableHandcuffs(ped, true)
+                        TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                     end 
                 end
             end 

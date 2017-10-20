8a1d300e2ef019ef5abdfdf979cf1da1a76d7881
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-20 10:27:32 -0300

    Found weapon aim and radio workaround

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -1,4 +1,3 @@
-
 function loadAnimDict( dict )
     while ( not HasAnimDictLoaded( dict ) ) do
         RequestAnimDict( dict )
@@ -26,10 +25,51 @@ Citizen.CreateThread( function()
                         SetEnableHandcuffs(ped, false)
                     else
                         if ( IsDisabledControlJustPressed( 0, 19 ) ) then
+						ClearPedSecondaryTask(ped)
                         TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
 						SetEnableHandcuffs(ped, true)
                     end 
 					   if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
+					   DisableControlAction(1, 140, true)
+					   DisableControlAction(1, 141, true)
+					   DisableControlAction(1, 142, true)
+					end
+                end
+            end 
+        end 
+    end
+end )
+
+function loadAnimDict( dict )
+    while ( not HasAnimDictLoaded( dict ) ) do
+        RequestAnimDict( dict )
+        Citizen.Wait( 0 )
+    end
+end
+
+Citizen.CreateThread( function()
+    while true do 
+        Citizen.Wait( 0 )
+
+        local ped = GetPlayerPed( -1 )
+
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+            DisableControlAction( 0, 36, true ) -- INPUT_DUCK (LEFTCTRL)  
+
+            if ( not IsPauseMenuActive() ) then 
+                    loadAnimDict( "random@arrests" )
+
+                    while ( not HasAnimDictLoaded( "random@arrests" ) ) do 
+                        Citizen.Wait( 100 )
+                    end 
+                        if ( IsDisabledControlJustReleased( 0, 36 ) ) then
+						ClearPedSecondaryTask(ped)
+                    else
+                        if ( IsDisabledControlJustPressed( 0, 36 ) ) then
+						ClearPedSecondaryTask(ped)
+                        TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
+                    end 
+					   if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
 					   DisableControlAction(1, 140, true)
 					   DisableControlAction(1, 141, true)
 					   DisableControlAction(1, 142, true)

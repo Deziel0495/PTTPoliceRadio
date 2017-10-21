992f6124a4c37628edef7f404624ea0eac0b30bb
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-21 14:14:16 -0300

    Added gun holster animation

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -41,7 +41,6 @@ Citizen.CreateThread( function()
                         SetEnableHandcuffs(ped, false)
                     else
                         if ( IsDisabledControlJustPressed( 0, 19 ) ) and checkskin() then
-                        ClearPedSecondaryTask(ped)
                         TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                         SetEnableHandcuffs(ped, true)
                     end 
@@ -82,7 +81,6 @@ Citizen.CreateThread( function()
                         ClearPedSecondaryTask(ped)
                     else
                         if ( IsDisabledControlJustPressed( 0, 36 ) ) then
-                        ClearPedSecondaryTask(ped)
                         TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                     end 
                        if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
@@ -90,6 +88,65 @@ Citizen.CreateThread( function()
                        DisableControlAction(1, 141, true)
                        DisableControlAction(1, 142, true)
                     end
+                end
+            end 
+        end 
+    end
+end )
+
+function loadAnimDict( dict )
+    while ( not HasAnimDictLoaded( dict ) ) do
+        RequestAnimDict( dict )
+        Citizen.Wait( 0 )
+    end
+end
+
+skins = {
+	-- Police --
+	GetHashKey("s_m_y_cop_01"),
+	GetHashKey("s_f_y_cop_01"),
+	-- Highway --
+	GetHashKey("s_m_y_hwaycop_01"),
+	-- Sheriff --
+	GetHashKey("s_m_y_sheriff_01"),
+	GetHashKey("s_f_y_sheriff_01"),
+	-- SECURITY --
+	GetHashKey("s_m_m_security_01"),
+	-- Ranger --
+	GetHashKey("s_m_y_ranger_01"),
+	GetHashKey("s_f_y_ranger_01"),
+}
+
+Citizen.CreateThread( function()
+    while true do 
+        Citizen.Wait( 0 )
+
+        local ped = GetPlayerPed( -1 )
+
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then 
+            DisableControlAction( 0, 20, true ) -- INPUT_MULTIPLAYER_INFO (Z)	
+
+            if ( not IsPauseMenuActive() ) then 
+                    loadAnimDict( "reaction@intimidation@cop@unarmed" )
+
+                    while ( not HasAnimDictLoaded( "reaction@intimidation@cop@unarmed" ) ) do 
+                        Citizen.Wait( 100 )
+                    end 
+                        if ( IsDisabledControlJustReleased( 0, 20 ) ) then
+						ClearPedSecondaryTask(ped)
+						SetEnableHandcuffs(ped, false)
+						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
+                    else
+                        if ( IsDisabledControlJustPressed( 0, 20 ) ) and checkskin() then
+						SetEnableHandcuffs(ped, true)
+						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true) 
+						TaskPlayAnim(ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
+                    end
+						if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "reaction@intimidation@cop@unarmed", "intro", 3) then 
+                       DisableControlAction(1, 140, true)
+                       DisableControlAction(1, 141, true)
+                       DisableControlAction(1, 142, true)
+					end	
                 end
             end 
         end 

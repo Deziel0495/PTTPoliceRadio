229920ce18a23704259887ddb0203653b5538b5c
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-23 17:18:47 -0300

    Update client.lua

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -34,14 +34,14 @@ Citizen.CreateThread(function()
                 while ( not HasAnimDictLoaded( "random@arrests" ) ) do 
                         Citizen.Wait( 100 )
                 end 
-                if ( IsControlJustReleased( 0, 19 ) ) then
+                if ( IsControlJustReleased( 0, 19 ) ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
                     ClearPedTasks(ped)
                     SetEnableHandcuffs(ped, false)
                 else
-                    if ( IsControlJustPressed( 0, 19 ) ) and checkskin() and not IsPlayerFreeAiming(PlayerId()) then
+                    if ( IsControlJustPressed( 0, 19 ) ) and checkskin() and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
                         TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                         SetEnableHandcuffs(ped, true)
-                    elseif ( IsControlJustPressed( 0, 19 ) ) and checkskin() and IsPlayerFreeAiming(PlayerId()) then
+                    elseif ( IsControlJustPressed( 0, 19 ) ) and checkskin() and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
                         TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
                     end 
                     if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
@@ -76,12 +76,12 @@ Citizen.CreateThread( function()
                     while ( not HasAnimDictLoaded( "reaction@intimidation@cop@unarmed" ) ) do 
                         Citizen.Wait( 100 )
                     end 
-                        if ( IsDisabledControlJustReleased( 0, 20 ) ) then
+                        if ( IsDisabledControlJustReleased( 0, 20 ) ) then -- INPUT_MULTIPLAYER_INFO (Z)
 						ClearPedTasks(ped)
 						SetEnableHandcuffs(ped, false)
 						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
                     else
-                        if ( IsDisabledControlJustPressed( 0, 20 ) ) and checkskin() then
+                        if ( IsDisabledControlJustPressed( 0, 20 ) ) and checkskin() then -- INPUT_MULTIPLAYER_INFO (Z)
 						SetEnableHandcuffs(ped, true)
 						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true) 
 						TaskPlayAnim(ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )

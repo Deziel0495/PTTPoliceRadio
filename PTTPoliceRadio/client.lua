b9dc95aa4df4d273898dc56d0513407135ee81bb
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-10-22 23:25:19 -0300

    Update client.lua

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -14,7 +14,7 @@ skins = {
 	GetHashKey("s_f_y_ranger_01"),
 }
 
---RADIO--
+-- RADIO --
 
 Citizen.CreateThread( function()
     while true do 
@@ -32,7 +32,7 @@ Citizen.CreateThread( function()
                         Citizen.Wait( 100 )
                     end 
                         if ( IsDisabledControlJustReleased( 0, 19 ) ) then
-                        ClearPedSecondaryTask(ped)
+                        ClearPedTasks(ped)
                         SetEnableHandcuffs(ped, false)
                     else
                         if ( IsDisabledControlJustPressed( 0, 19 ) ) and checkskin() then
@@ -50,7 +50,7 @@ Citizen.CreateThread( function()
     end
 end )
 
---RADIO W/ GUN AIMED--
+-- RADIO W/ GUN AIMED --
 
 Citizen.CreateThread( function()
     while true do 
@@ -68,7 +68,7 @@ Citizen.CreateThread( function()
                         Citizen.Wait( 100 )
                     end 
                         if ( IsDisabledControlJustReleased( 0, 36 ) ) then
-                        ClearPedSecondaryTask(ped)
+                        ClearPedTasks(ped)
                     else
                         if ( IsDisabledControlJustPressed( 0, 36 ) ) then
                         TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
@@ -84,7 +84,7 @@ Citizen.CreateThread( function()
     end
 end )
 
---GUN HOLSTER--
+-- GUN HOLSTER --
 
 Citizen.CreateThread( function()
     while true do 
@@ -102,7 +102,7 @@ Citizen.CreateThread( function()
                         Citizen.Wait( 100 )
                     end 
                         if ( IsDisabledControlJustReleased( 0, 20 ) ) then
-						ClearPedSecondaryTask(ped)
+						ClearPedTasks(ped)
 						SetEnableHandcuffs(ped, false)
 						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
                     else

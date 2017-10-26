27353dff4407fd7b35391660a9953efbd5f922ac
JinkLeft <JinkLeft@users.noreply.github.com>
2017-10-26 13:54:28 -0500

    Add files via upload
    ADDED: Weapons table which should include all the pistols and can just be commented out if they don't want those to work. -- JINK
    MODIFIED: Spacing of the client.lua so that it should reflect accurately on github now.
    ADDED: functions to check for weapons and moved disabling actions to a function.
    BUGS: I added disabling firing of weapons to the function because even though you set the peds to unarmed they could still fire their weapons when talking on radio. This should fix that. --JINK

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -8,136 +8,141 @@
 -- RESTRICTED PEDS --
 -- I've only listed peds that have a remote speaker mic, but any ped listed here will do the animations.
 
-skins = {
+local skins = {
 	-- Police --
-	GetHashKey("s_m_y_cop_01"),
-	GetHashKey("s_f_y_cop_01"),
+	"s_m_y_cop_01",
+	"s_f_y_cop_01",
 	-- Highway --
-	GetHashKey("s_m_y_hwaycop_01"),
+	"s_m_y_hwaycop_01",
 	-- Sheriff --
-	GetHashKey("s_m_y_sheriff_01"),
-	GetHashKey("s_f_y_sheriff_01"),
+	"s_m_y_sheriff_01",
+	"s_f_y_sheriff_01",
 	-- Security --
-	GetHashKey("s_m_m_security_01"),
+	"s_m_m_security_01",
 	-- Ranger --
-	GetHashKey("s_m_y_ranger_01"),
-	GetHashKey("s_f_y_ranger_01"),
+	"s_m_y_ranger_01",
+	"s_f_y_ranger_01",
+}
+
+-- Add weapon hashes here to be added for holster checks.
+local weapons = {
+	"WEAPON_MARKSMANPISTOL",
+	"WEAPON_PISTOL",
+	"WEAPON_REVOLVER",
+	"WEAPON_STUNGUN",
+	"WEAPON_COMBATPISTOL",
+	"WEAPON_PISTOL50",
+	"WEAPON_APPISTOL",
+	"WEAPON_HEAVYPISTOL",
+	"WEAPON_SNSPISTOL",
 }
 
 -- RADIO ANIMATIONS --
 
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait( 0 )
-
-        local ped = PlayerPedId()
-
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then
-		
-			if ( not IsPauseMenuActive() ) then 
-                loadAnimDict( "random@arrests" )
-
-                while ( not HasAnimDictLoaded( "random@arrests" ) ) do 
-                        Citizen.Wait( 100 )
-                end 
-                if ( IsControlJustReleased( 0, 19 ) ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
-                    ClearPedTasks(ped)
-                    SetEnableHandcuffs(ped, false)
-                else
-                    if ( IsControlJustPressed( 0, 19 ) ) and checkskin() and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+	while true do
+		Citizen.Wait( 0 )
+		local ped = PlayerPedId()
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and CheckSkin(ped) then
+			if not IsPauseMenuActive() then 
+				loadAnimDict( "random@arrests" )
+				if IsControlJustReleased( 0, 19 ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+					ClearPedTasks(ped)
+					SetEnableHandcuffs(ped, false)
+				else
+					if IsControlJustPressed( 0, 19 )and CheckSkin(ped) and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 						TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-                        SetEnableHandcuffs(ped, true)
-                    elseif ( IsControlJustPressed( 0, 19 ) ) and checkskin() and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+						SetEnableHandcuffs(ped, true)
+					elseif IsControlJustPressed( 0, 19 ) and CheckSkin(ped) and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 						TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-                    end 
-                    if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
-                        DisableControlAction(1, 140, true)
-                        DisableControlAction(1, 141, true)
-                        DisableControlAction(1, 142, true)
-                    elseif IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
-                        DisableControlAction(1, 140, true)
-                        DisableControlAction(1, 141, true)
-                        DisableControlAction(1, 142, true)
-                    end
-                end
-            end 
-        end 
-    end
+					end 
+					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
+						DisableActions(ped)
+					elseif IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "radio_chatter", 3) then
+						DisableActions(ped)
+					end
+				end
+			end 
+		end 
+	end
 end )
 
 -- HOLD WEAPON HOLSTER ANIMATION --
 
 Citizen.CreateThread( function()
-    while true do 
-        Citizen.Wait( 0 )
-
-        local ped = PlayerPedId()
-
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then 
+	while true do 
+		Citizen.Wait( 0 )
+		local ped = PlayerPedId()
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and CheckSkin(ped) then 
 			DisableControlAction( 0, 20, true ) -- INPUT_MULTIPLAYER_INFO (Z)
-
-			if ( not IsPauseMenuActive() ) then 
-                    loadAnimDict( "reaction@intimidation@cop@unarmed" )
-
-                    while ( not HasAnimDictLoaded( "reaction@intimidation@cop@unarmed" ) ) do 
-                        Citizen.Wait( 100 )
-                    end 
-                        if ( IsDisabledControlJustReleased( 0, 20 ) ) then -- INPUT_MULTIPLAYER_INFO (Z)
-						ClearPedTasks(ped)
-						SetEnableHandcuffs(ped, false)
-						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
-                    else
-                        if ( IsDisabledControlJustPressed( 0, 20 ) ) and checkskin() then -- INPUT_MULTIPLAYER_INFO (Z)
+			if not IsPauseMenuActive() then 
+				loadAnimDict( "reaction@intimidation@cop@unarmed" )
+				if IsDisabledControlJustReleased( 0, 20 ) then -- INPUT_MULTIPLAYER_INFO (Z)
+					ClearPedTasks(ped)
+					SetEnableHandcuffs(ped, false)
+					SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
+				else
+					if IsDisabledControlJustPressed( 0, 20 ) and CheckSkin(ped) then -- INPUT_MULTIPLAYER_INFO (Z)
 						SetEnableHandcuffs(ped, true)
 						SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true) 
 						TaskPlayAnim(ped, "reaction@intimidation@cop@unarmed", "intro", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
-                    end
-						if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "reaction@intimidation@cop@unarmed", "intro", 3) then 
-                       DisableControlAction(1, 140, true)
-                       DisableControlAction(1, 141, true)
-                       DisableControlAction(1, 142, true)
+					end
+					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "reaction@intimidation@cop@unarmed", "intro", 3) then 
+						DisableActions(ped)
 					end	
-                end
-            end 
-        end 
-    end
+				end
+			end 
+		end 
+	end
 end )
 
 -- HOLSTER/UNHOLSTER PISTOL --
  
  Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(0)
-			
-			local ped = PlayerPedId()
-			
-        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) and not IsPedInAnyVehicle(PlayerPedId(), true) and checkskin() then
-            
-			if GetSelectedPedWeapon(ped) == GetHashKey("WEAPON_PISTOL") then
-                    SetPedComponentVariation(ped, 9, 0, 0, 0)
-				else
-				if GetSelectedPedWeapon(ped) ~= GetHashKey("WEAPON_PISTOL") then
-					SetPedComponentVariation(ped, 9, 1, 0, 0)
-                end
-            end
-        end
-    end
+	while true do
+		Citizen.Wait(0)
+		local ped = PlayerPedId()
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and CheckSkin(ped) then
+			if CheckWeapon(ped) then
+				SetPedComponentVariation(ped, 9, 0, 0, 0)
+			elseif not CheckWeapon(ped) then
+				SetPedComponentVariation(ped, 9, 1, 0, 0)
+			end
+		end
+	end
 end)
 
 -- DO NOT REMOVE THESE! --
 
-function checkskin()
-    for i = 1, #skins do
-        if skins[i] == GetEntityModel(PlayerPedId()) then
-            return true
-        end
-    end
-    return false
+function CheckSkin(ped)
+	for i = 1, #skins do
+		if GetHashKey(skins[i]) == GetEntityModel(ped) then
+			return true
+		end
+	end
+	return false
+end
+
+function CheckWeapon(ped)
+	for i = 1, #weapons do
+		if GetHashKey(weapons[i]) == GetSelectedPedWeapon(ped) then
+			return true
+		end
+	end
+	return false
+end
+
+function DisableActions(ped)
+	DisableControlAction(1, 140, true)
+	DisableControlAction(1, 141, true)
+	DisableControlAction(1, 142, true)
+	DisableControlAction(1, 37, true) --Disables INPUT_SELECT_WEAPON (tab) Actions
+	DisablePlayerFiring(ped, true) -- Disable weapon firing
 end
 
 function loadAnimDict( dict )
-    while ( not HasAnimDictLoaded( dict ) ) do
-        RequestAnimDict( dict )
-        Citizen.Wait( 0 )
-    end
+	while ( not HasAnimDictLoaded( dict ) ) do
+		RequestAnimDict( dict )
+		Citizen.Wait( 0 )
+	end
 end

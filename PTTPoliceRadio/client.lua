26dea87ffaa9c62587d7217a9aa351f5f682287b
Daniel Deziel <32435437+Deziel0495@users.noreply.github.com>
2017-11-13 00:53:38 -0400

    Update client.lua

--- PTTPoliceRadio/client.lua
+++ PTTPoliceRadio/client.lua
@@ -11,30 +11,19 @@ local holstered = true
 -- RESTRICTED PEDS --
 -- I've only listed peds that have a remote speaker mic, but any ped listed here will do the animations.
 local skins = {
-	-- Police --
 	"s_m_y_cop_01",
 	"s_f_y_cop_01",
-	-- Highway --
 	"s_m_y_hwaycop_01",
-	-- Sheriff --
 	"s_m_y_sheriff_01",
 	"s_f_y_sheriff_01",
-	-- Ranger --
 	"s_m_y_ranger_01",
 	"s_f_y_ranger_01",
 }
 
 -- Add/remove weapon hashes here to be added for holster checks.
 local weapons = {
-	"WEAPON_MARKSMANPISTOL",
 	"WEAPON_PISTOL",
-	"WEAPON_REVOLVER",
-	"WEAPON_STUNGUN",
 	"WEAPON_COMBATPISTOL",
-	"WEAPON_PISTOL50",
-	"WEAPON_APPISTOL",
-	"WEAPON_HEAVYPISTOL",
-	"WEAPON_SNSPISTOL",
 }
 
 -- RADIO ANIMATIONS --
@@ -43,7 +32,7 @@ Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait( 0 )
 		local ped = PlayerPedId()
-		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) and CheckSkin(ped) then
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and CheckSkin(ped) then
 			if not IsPauseMenuActive() then 
 				loadAnimDict( "random@arrests" )
 				if IsControlJustReleased( 0, 19 ) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
@@ -51,13 +40,14 @@ Citizen.CreateThread(function()
 					ClearPedTasks(ped)
 					SetEnableHandcuffs(ped, false)
 				else
-					if IsControlJustPressed( 0, 19 )and CheckSkin(ped) and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
+					if IsControlJustPressed( 0, 19 ) and CheckSkin(ped) and not IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 						TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
 						TaskPlayAnim(ped, "random@arrests", "generic_radio_enter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
 						SetEnableHandcuffs(ped, true)
 					elseif IsControlJustPressed( 0, 19 ) and CheckSkin(ped) and IsPlayerFreeAiming(PlayerId()) then -- INPUT_CHARACTER_WHEEL (LEFT ALT)
 						TriggerServerEvent('InteractSound_SV:PlayOnSource', 'on', 0.1)
 						TaskPlayAnim(ped, "random@arrests", "radio_chatter", 8.0, 2.0, -1, 50, 2.0, 0, 0, 0 )
+						SetEnableHandcuffs(ped, true)
 					end 
 					if IsEntityPlayingAnim(GetPlayerPed(PlayerId()), "random@arrests", "generic_radio_enter", 3) then
 						DisableActions(ped)
@@ -151,7 +141,7 @@ function DisableActions(ped)
 	DisableControlAction(1, 140, true)
 	DisableControlAction(1, 141, true)
 	DisableControlAction(1, 142, true)
-	DisableControlAction(1, 37, true) --Disables INPUT_SELECT_WEAPON (tab) Actions
+	DisableControlAction(1, 37, true) -- Disables INPUT_SELECT_WEAPON (TAB)
 	DisablePlayerFiring(ped, true) -- Disable weapon firing
 end
 
